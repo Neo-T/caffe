336b2e1ca5b4f431c4a9ac86a0a8123794a96427
Neo-T <marsstory99@qq.com>
2018-10-18 17:13:24 +0800

    Add MS VS2015 support

--- python/caffe/_caffe.cpp
+++ python/caffe/_caffe.cpp
@@ -39,9 +39,73 @@
   } \
 } while (0)
 
+#if defined(_MSC_VER) && (_MSC_FULL_VER >= 190024210)
+// Workaround for VS 2015 Update 3 which breaks boost python
+// See: http://stackoverflow.com/questions/38261530/unresolved-external-symbols-since-visual-studio-2015-update-3-boost-python-link
+// and https://msdn.microsoft.com/vs-knownissues/vs2015-update3
+#define BP_GET_POINTER(cls) \
+namespace boost { \
+template <> \
+const volatile caffe::cls * \
+get_pointer(const volatile caffe::cls *c) { \
+    return c; \
+} \
+}
+
+#define BP_GET_POINTER_T(cls, dtype) BP_GET_POINTER(cls<dtype>)
+
+BP_GET_POINTER_T(Net, float);
+BP_GET_POINTER_T(Layer, float);
+BP_GET_POINTER_T(Solver, float);
+BP_GET_POINTER_T(SGDSolver, float);
+BP_GET_POINTER_T(NesterovSolver, float);
+BP_GET_POINTER_T(AdaGradSolver, float);
+BP_GET_POINTER_T(RMSPropSolver, float);
+BP_GET_POINTER_T(AdaDeltaSolver, float);
+BP_GET_POINTER_T(AdamSolver, float);
+
+#endif
+
 namespace bp = boost::python;
 
+#include "caffe/layers/input_layer.hpp"
+#include "caffe/layers/inner_product_layer.hpp"
+#include "caffe/layers/dropout_layer.hpp"
+#include "caffe/layers/conv_layer.hpp"
+#include "caffe/layers/relu_layer.hpp"
+
+#include "caffe/layers/pooling_layer.hpp"
+#include "caffe/layers/lrn_layer.hpp"
+#include "caffe/layers/softmax_layer.hpp"
+#include "caffe/layers/permute_layer.hpp"
+#include "caffe/layers/flatten_layer.hpp"
+#include "caffe/layers/prior_box_layer.hpp"
+#include "caffe/layers/concat_layer.hpp"
+#include "caffe/layers/reshape_layer.hpp"
+#include "caffe/layers/detection_output_layer.hpp"
+
 namespace caffe {
+extern INSTANTIATE_CLASS(InputLayer);
+extern INSTANTIATE_CLASS(InnerProductLayer);
+extern INSTANTIATE_CLASS(DropoutLayer);
+extern INSTANTIATE_CLASS(MemoryDataLayer);
+extern INSTANTIATE_CLASS(ConvolutionLayer);
+REGISTER_LAYER_CLASS(Convolution);
+extern INSTANTIATE_CLASS(ReLULayer);
+REGISTER_LAYER_CLASS(ReLU);
+extern INSTANTIATE_CLASS(PoolingLayer);
+REGISTER_LAYER_CLASS(Pooling);
+extern INSTANTIATE_CLASS(LRNLayer);
+REGISTER_LAYER_CLASS(LRN);
+extern INSTANTIATE_CLASS(SoftmaxLayer);
+REGISTER_LAYER_CLASS(Softmax);
+extern INSTANTIATE_CLASS(PermuteLayer);
+extern INSTANTIATE_CLASS(FlattenLayer);
+extern INSTANTIATE_CLASS(PriorBoxLayer);
+extern INSTANTIATE_CLASS(ConcatLayer);
+extern INSTANTIATE_CLASS(ReshapeLayer);
+extern INSTANTIATE_CLASS(DetectionOutputLayer);
+
 
 // For Python, for now, we'll just always use float as the type.
 typedef float Dtype;
